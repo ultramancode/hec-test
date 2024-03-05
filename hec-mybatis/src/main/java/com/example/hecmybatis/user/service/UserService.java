fa0f125db406e3c3108113096a5afc73ab9caeae
kimtaewoong <ktw2172@gmail.com>
2024-03-05 22:42:34 +0900

    feat: UserService 유저 생성, 수정, 소프트딜리트, 조회

--- hec-mybatis/src/main/java/com/example/hecmybatis/user/service/UserService.java
+++ hec-mybatis/src/main/java/com/example/hecmybatis/user/service/UserService.java
@@ -1,13 +1,18 @@
 package com.example.hecmybatis.user.service;
 
 import com.example.heccore.user.model.UserVO;
+import com.example.hecmybatis.user.dto.request.UserConditionDto;
 import com.example.hecmybatis.user.dto.request.UserNameUpdateRequestDto;
+import com.example.hecmybatis.user.dto.request.UserRequestDto;
 import com.example.hecmybatis.user.dto.response.UserResponseDto;
 import com.example.hecmybatis.user.mapper.UserMapper;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,27 +20,58 @@ public class UserService {
 
     private final UserMapper userMapper;
 
-    public List<UserResponseDto> getAllUsers() {
-        List<UserVO> userVOS = userMapper.selectAllUsers();
-
-        return userVOS.stream().map(userVO ->
-                    new UserResponseDto(userVO.getUserId(), userVO.getName())).collect(Collectors.toList());
-    }
-    public UserVO getUserVOById(Long userId) {
-        return userMapper.getUserById(userId);
+    @Transactional
+    public void createUser(UserRequestDto userRequestDto) {
+        UserVO userVO = new UserVO(userRequestDto.name());
+        userMapper.createUser(userVO);
     }
 
+    @Transactional
     public void updateUserName(Long userId, UserNameUpdateRequestDto userNameUpdateRequestDto) {
-        UserVO user = getUserVOById(userId);
+        UserVO userVO = getUserVOById(userId);
 
         // 사용자 정보가 null이 아닌 경우에만 이름을 업데이트
-        if (user != null) {
+        if (userVO != null) {
             // 업데이트할 사용자 이름 설정
-            user.updateUserName(userNameUpdateRequestDto.name());
+            userVO.updateUserName(userNameUpdateRequestDto.name());
             // 사용자 정보 업데이트
-            userMapper.updateUserName(user);
+            userMapper.updateUserName(userVO);
         } else {
             // 커스텀 익셉션 추가하자
         }
     }
+
+    @Transactional
+    public void softDeleteUser(Long userId) {
+        UserVO userVO = getUserVOById(userId);
+
+        // 사용자 정보가 null이 아닌 경우에만 소프트 딜리트
+        if (userVO != null) {
+            userVO.softDelete();
+            userMapper.softDeleteUser(userVO);
+        } else {
+            // 커스텀 익셉션 추가하자
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponseDto getUser(Long userId) {
+        UserVO userVO = getUserVOById(userId);
+        return new UserResponseDto(userVO.getUserId(), userVO.getName());
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponseDto> getUsersWithOptions(UserConditionDto userConditionDto) {
+
+        List<UserVO> userVOS = userMapper.getUsersWithOptions(userConditionDto);
+
+
+        return userVOS.stream().map(userVO ->
+                new UserResponseDto(userVO.getUserId(), userVO.getName())).collect(Collectors.toList());
+    }
+
+    public UserVO getUserVOById(Long userId) {
+        return userMapper.getUserById(userId);
+    }
+
 }
