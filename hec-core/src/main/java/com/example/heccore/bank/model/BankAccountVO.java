89d1b7f1bdbf6e6796fc84b84a7793453bbc993e
taewoongKim <ktw2172@gmail.com>
2024-03-06 08:44:22 +0900

    feat: BankAccountVO 입,출금 메소드 추가

--- hec-core/src/main/java/com/example/heccore/bank/model/BankAccountVO.java
+++ hec-core/src/main/java/com/example/heccore/bank/model/BankAccountVO.java
@@ -1,11 +1,14 @@
 package com.example.heccore.bank.model;
 
 import com.example.heccore.common.enums.Bank;
+import com.example.heccore.common.exception.ErrorCode;
+import com.example.heccore.common.exception.HecCustomException;
 import com.example.heccore.common.model.BaseVO;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,15 +23,32 @@ public class BankAccountVO extends BaseVO {
     private BigDecimal balance;
     private boolean isDeleted = false;
 
-    public BankAccountVO(
-            Long accountId, Long userId, Bank bank, Long accountNumber, BigDecimal balance) {
-        this.accountId = accountId;
+    @Builder
+    public BankAccountVO(Long userId, Bank bank, Long accountNumber, BigDecimal balance) {
         this.userId = userId;
         this.bank = bank;
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
+    // 입금 메소드
+    public void deposit(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new HecCustomException(ErrorCode.DEPOSIT_IS_NOT_VALID);
+        }
+        this.balance = this.balance.add(amount);
+    }
+
+    // 출금 메소드
+    public void withdraw(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new HecCustomException(ErrorCode.WITHDRAW_IS_NOT_VALID);
+        }
+        if (amount.compareTo(this.balance) > 0) {
+            throw new HecCustomException(ErrorCode.BALANCE_IS_NOT_VALID);
+        }
+        this.balance = this.balance.subtract(amount);
+    }
     public void softDelete() {
         this.isDeleted = true;
     }
