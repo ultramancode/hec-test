c528ae6749a3f3f5a27d81c14c0ee5ea7022db49
taewoongKim <ktw2172@gmail.com>
2024-03-06 08:46:01 +0900

    fix: xml 쿼리 수정

--- hec-mybatis/src/main/java/com/example/hecmybatis/bankAccount/mapper/BankAccountMapper.java
+++ hec-mybatis/src/main/java/com/example/hecmybatis/bankAccount/mapper/BankAccountMapper.java
@@ -3,6 +3,7 @@ package com.example.hecmybatis.bankAccount.mapper;
 import com.example.heccore.bank.model.BankAccountVO;
 import com.example.heccore.bank.model.BankAccountWithUserNameVO;
 
+import com.example.hecmybatis.bankAccount.dto.request.BankAccountConditionDto;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -10,8 +11,16 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface BankAccountMapper {
 
-    @Select("SELECT * FROM bank_account")
-    List<BankAccountVO> selectAllBankAccounts();
+    void createBankAccount(BankAccountVO bankAccountVO);
+    void updateBalance(BankAccountVO bankAccountVO);
 
-    List<BankAccountWithUserNameVO> getBankAccountsWithUser();
+    void softDeleteBankAccount(BankAccountVO bankAccountVO);
+
+    BankAccountVO getBankAccountById(Long userId);
+
+    List<BankAccountWithUserNameVO> getBankAccountsByUserId(Long userId);
+
+    List<BankAccountWithUserNameVO> getBankAccountsWithUserNameAndOptions(BankAccountConditionDto bankAccountConditionDto);
+
+    boolean isBankAccountNumberExists(Long bankAccountNumber);
 }
