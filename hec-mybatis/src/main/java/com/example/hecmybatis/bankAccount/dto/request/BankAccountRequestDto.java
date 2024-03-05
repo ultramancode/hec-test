0bfd802ee1d36f1092f7efbc0806de1537532fb9
taewoongKim <ktw2172@gmail.com>
2024-03-06 08:42:26 +0900

    feat: BankAccountRequestDto

---
+++ hec-mybatis/src/main/java/com/example/hecmybatis/bankAccount/dto/request/BankAccountRequestDto.java
@@ -0,0 +1,8 @@
+package com.example.hecmybatis.bankAccount.dto.request;
+
+import com.example.heccore.common.enums.Bank;
+import java.math.BigDecimal;
+
+public record BankAccountRequestDto(Long userId, Bank bank, BigDecimal balance) {
+
+}
