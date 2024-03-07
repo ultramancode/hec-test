11195741b7858fd9e3144aa308e8b3e3d9134e0a
taewoongKim <ktw2172@gmail.com>
2024-03-07 09:07:00 +0900

    feat: UserService 통합 테스트 조회 api (페이징,검색,정렬)

--- hec-mybatis/src/test/java/com/example/hecmybatis/user/integrationTest/UserServiceIntegrationTest.java
+++ hec-mybatis/src/test/java/com/example/hecmybatis/user/integrationTest/UserServiceIntegrationTest.java
@@ -1,7 +1,9 @@
 package com.example.hecmybatis.user.integrationTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import com.example.heccore.common.enums.Bank;
-import com.example.heccore.user.model.UserVO;
 import com.example.hecmybatis.bankAccount.dto.request.BankAccountRequestDto;
 import com.example.hecmybatis.bankAccount.mapper.BankAccountMapper;
 import com.example.hecmybatis.bankAccount.service.BankAccountService;
@@ -11,23 +13,14 @@ import com.example.hecmybatis.user.dto.request.UserRequestDto;
 import com.example.hecmybatis.user.dto.response.UserResponseDto;
 import com.example.hecmybatis.user.mapper.UserMapper;
 import com.example.hecmybatis.user.service.UserService;
-import org.junit.jupiter.api.Assertions;
+import java.math.BigDecimal;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.iterable;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -79,7 +72,7 @@ public class UserServiceIntegrationTest {
 
     @Test
     @DisplayName("유저 소프트 딜리트 테스트")
-    void softDeleteUser_Success() {
+    void softDeleteUser() {
         // given
         UserRequestDto userRequestDto = new UserRequestDto("김태웅");
         Long userId = userService.createUser(userRequestDto);
@@ -96,7 +89,7 @@ public class UserServiceIntegrationTest {
 
     @Test
     @DisplayName("유저 단건 조회 테스트")
-    void getUser_Success() {
+    void getUser() {
         // given
         UserRequestDto userRequestDto = new UserRequestDto("김태웅");
         Long userId = userService.createUser(userRequestDto);
@@ -112,41 +105,67 @@ public class UserServiceIntegrationTest {
     }
 
     @Test
-    @DisplayName("유저 다양한 조건 조회 테스트")
-    void getUsersWithOptions_Success() {
+    @DisplayName("유저 다양한 조건 조회 테스트(페이징)")
+    void getUsersWithPaging() {
+        //페이징 테스트
         // given
         for (int i = 1; i <= 10; i++) {
-            UserRequestDto userRequestDto = new UserRequestDto("김태웅"+i);
+            UserRequestDto userRequestDto = new UserRequestDto("김태웅" + i);
             userService.createUser(userRequestDto);
         }
-        UserRequestDto userRequestDto = new UserRequestDto("박모씨");
-        userService.createUser(userRequestDto);
 
-        UserConditionDto userConditionDto = new UserConditionDto(2,3);
+        UserConditionDto userConditionDto = new UserConditionDto(2, 3);
 
         // when
         List<UserResponseDto> userResponseDtos = userService.getUsersWithOptions(userConditionDto);
 
-        // then
+        // then (페이징 테스트)
         // 사이즈 3일 때 2페이지의 첫번째 유저는 유저 아이디가 4여야 하니까
         assertThat(userService.getUser(4L).userId()).isEqualTo(userResponseDtos.get(0).userId());
-
-//        //given
-//        userConditionDto.setName("박");
-//
-//        //when
-//        List<UserResponseDto> userResponseDtosSecond = userService.getUsersWithOptions(userConditionDto);
-//        System.out.println(userResponseDtosSecond.size());
-//        for (UserResponseDto u: userResponseDtosSecond
-//             ) {
-//            System.out.println(u.name());
-//        }
-//
-//        //then
-//        assertThat(userService.getUser(5L).userId()).isEqualTo(userResponseDtosSecond.get(0).userId());
+    }
 
 
-        // Additional assertions if needed
+    @Test
+    @DisplayName("유저 다양한 조건 조회 테스트(검색)")
+    void getUsersWithSearch() {
+        //given
+        UserRequestDto userRequestDto = new UserRequestDto("김박태웅");
+        Long userId = userService.createUser(userRequestDto);
+        UserRequestDto userRequestDtoSecond = new UserRequestDto("최이태웅");
+        Long userIdSecond = userService.createUser(userRequestDtoSecond);
+
+        UserConditionDto userConditionDto = new UserConditionDto(1, 3);
+        userConditionDto.setName("이");
+
+        //when
+        List<UserResponseDto> userResponseDtosWithSearch = userService.getUsersWithOptions(
+                userConditionDto);
+
+        //then (검색 기능 테스트)
+        assertThat(userService.getUser(userIdSecond).name()).isEqualTo(
+                userResponseDtosWithSearch.get(0).name());
+    }
+
+    @Test
+    @DisplayName("유저 다양한 조건 조회 테스트(정렬)")
+    void getUsersWithOrderBy() {
+        //given
+        UserRequestDto userRequestDto = new UserRequestDto("김태웅1");
+        Long userId = userService.createUser(userRequestDto);
+        UserRequestDto userRequestDtoSecond = new UserRequestDto("김태웅2");
+        Long userIdSecond = userService.createUser(userRequestDtoSecond);
+
+        UserConditionDto userConditionDto = new UserConditionDto(1, 3);
+
+        userConditionDto.setOrderBy("name");
+        userConditionDto.setOrderDirection("desc");
+
+        //when
+        List<UserResponseDto> userResponseDtosWithOrderBy = userService.getUsersWithOptions(
+                userConditionDto);
+
+        assertThat(userService.getUser(userIdSecond).userId()).isEqualTo(
+                userResponseDtosWithOrderBy.get(0).userId());
     }
 }
 
