a0db5301b68bde67bb5700bde9eac3b53786b3ba
kimtaewoong <ktw2172@gmail.com>
2024-03-09 23:27:22 +0900

    fix: 스프링 배치 모듈 -> 기존 따로 서버 띄워지는 방식에서 mybatis-api 모듈에서 실행되도록 수정

--- hec-batch/src/main/java/com/example/hecbatch/HecBatchApplication.java
+++ hec-batch/src/main/java/com/example/hecbatch/HecBatchApplication.java
@@ -1,13 +1,17 @@
-package com.example.hecbatch;
+/**
+ * 추후 배치서버 별도 분리 시, 설정 필요. 지금은 api 모듈에서 실행 중이니 필요 없음
+ */
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class HecBatchApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HecBatchApplication.class, args);
-    }
-
-}
+//package com.example.hecbatch;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class HecBatchApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(HecBatchApplication.class, args);
+//    }
+//
+//}
