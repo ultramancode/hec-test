10742eb667abe937429076e15c5388d9a606db02
kimtaewoong <ktw2172@gmail.com>
2024-03-10 22:16:17 +0900

    feat: 웹플럭스 r2dbc용 유저 엔티티 생성

--- hec-webflux/src/main/java/com/example/hecwebflux/HecWebfluxApplication.java
+++ hec-webflux/src/main/java/com/example/hecwebflux/HecWebfluxApplication.java
@@ -2,8 +2,10 @@ package com.example.hecwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @SpringBootApplication
+@EnableR2dbcAuditing
 public class HecWebfluxApplication {
 
     public static void main(String[] args) {
---
+++ hec-webflux/src/main/java/com/example/hecwebflux/user/entity/User.java
@@ -0,0 +1,50 @@
+package com.example.hecwebflux.user.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.annotation.processing.Generated;
+import java.time.LocalDateTime;
+@Table("users")
+@NoArgsConstructor
+@Setter
+@Getter
+public class User {
+    @Id
+    private Long userId;
+    private String name;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    private boolean isDeleted;
+
+    public User(String name) {
+        this.name = name;
+        this.isDeleted = false;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", isDeleted=" + isDeleted +
+                '}';
+    }
+
+    public void softDelete() {
+        this.isDeleted = true;
+    }
+    public void updateUserName(String name){
+        this.name = name;
+    }
+
+}
