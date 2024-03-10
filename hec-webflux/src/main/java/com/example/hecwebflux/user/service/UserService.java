d463ac0738c5e4d8e21f14ccbdc5965cdebcef22
kimtaewoong <ktw2172@gmail.com>
2024-03-11 08:19:56 +0900

    feat: databaseClient 방식으로 전체 유저 조회

--- hec-webflux/src/main/java/com/example/hecwebflux/user/service/UserService.java
+++ hec-webflux/src/main/java/com/example/hecwebflux/user/service/UserService.java
@@ -9,6 +9,7 @@ import com.example.hecwebflux.user.entity.User;
 import com.example.hecwebflux.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -19,6 +20,7 @@ import reactor.core.publisher.Mono;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final DatabaseClient databaseClient;
 
     public Mono<User> createUser(UserRequestDto userRequestDto) {
         User user = new User(userRequestDto.name());
@@ -54,6 +56,12 @@ public class UserService {
     public Flux<UserResponseDto> getAllUsers() {
         return userRepository.findUsersByIsDeletedIsFalse()
                 .map(user -> new UserResponseDto(user.getUserId(), user.getName()));
+    }
+
+    public Flux<UserResponseDto> getAllUsersByNativeQuery() {
+        return databaseClient.sql("SELECT * FROM users WHERE is_deleted = FALSE")
+                .map(row -> new UserResponseDto((Long) row.get("user_id"), (String) row.get("name")))
+                .all();
     }
 
     public Mono<User> getUserById(Long userId) {
